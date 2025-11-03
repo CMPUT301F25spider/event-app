c30c0657e66e5d9d4e32d32a884ee75dfdbaef5b
sleepypant <ani.so0tropically@gmail.com>
2025-11-03 10:58:35 -0700

    Added settings functionality

--- app/src/main/java/com/example/event_app/MainActivity.java
+++ app/src/main/java/com/example/event_app/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.event_app;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void toggle(View v) {
+       v.setEnabled(false);
+       Log.d("successbro", "button disabled"); //loggging
+        Button button = (Button) v;
+        button.setText("Disabled");
+    }
+    public void handleText(View v){
+
+
+        Toast.makeText(this,"hi", Toast.LENGTH_LONG).show();
+        Log.d("input", "hi");
+
+    }
+    public void launchSettings(View v){
+        //launch a new activity
+        Intent i = new Intent(this,SettingsActivity.class );
+        startActivity(i);
+
     }
 }
